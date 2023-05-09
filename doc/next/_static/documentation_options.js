d8b6f879ba95af3e9a3b4d477e77d45b7cc507f9
Piotr Makowski <piotr.makowski.ext@adevinta.com>
2023-05-09 14:52:07 +0200

    new version released

--- doc/next/_static/documentation_options.js
+++ doc/next/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.126',
+    VERSION: '0.127',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
